32a33be64ca224001eafe30412bbe0e93adc71df
JorgeYael-CB <jorge.yael333@gmail.com>
2024-05-14 09:00:43 -0600

    envs.ts actualizado

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,4 +5,5 @@ import { get } from 'env-var';
 export const envs = {
     PORT: get('PORT').required().asPortNumber(),
     KEY_SECRET_SRTIPE: get('KEY_SECRET_SRTIPE').required().asString(),
+    KEY_HOOK_STRIPE: get('KEY_HOOK_STRIPE').required().asString(),
 };
